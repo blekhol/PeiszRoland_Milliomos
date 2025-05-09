0519bc5c20c829baff714389de69f00e0362c288
Petrik Diak <diak@petrik.hu>
2025-05-09 11:17:23 +0200

    sima és sorkérdés osztály

---
+++ millionos/Kerdes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace millionos
+{
+	internal class Kerdes
+	{
+		private string kerdesSzoveg;
+		private List<string> valaszok;
+		private int helyes;
+		private string kategoria;
+
+		public Kerdes(string kerdes, List<string> valaszok, int helyes, string kategoria)
+		{
+			this.kerdesSzoveg = kerdes;
+			this.valaszok = valaszok;
+			this.helyes = helyes;
+			this.kategoria = kategoria;
+		}
+
+		public string KerdesSzoveg { get => kerdesSzoveg; set => kerdesSzoveg = value; }
+		public List<string> Valaszok { get => valaszok; set => valaszok = value; }
+		public int Helyes { get => helyes; set => helyes = value; }
+		public string Kategoria { get => kategoria; set => kategoria = value; }
+	}
+}
---
+++ millionos/Sorkerdes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace millionos
+{
+	internal class Sorkerdes
+	{
+		private string kerdesSzoveg;
+		private List<string> valaszok;
+		private Dictionary<int, string> helyesSorrend;
+		private string kategoria;
+
+		public Sorkerdes(string kerdesSzoveg, List<string> valaszok, Dictionary<int, string> helyesSorrend, string kategoria)
+		{
+			this.kerdesSzoveg = kerdesSzoveg;
+			this.valaszok = valaszok;
+			this.helyesSorrend = helyesSorrend;
+			this.kategoria = kategoria;
+		}
+
+		public string KerdesSzoveg { get => kerdesSzoveg; set => kerdesSzoveg = value; }
+		public List<string> Valaszok { get => valaszok; set => valaszok = value; }
+		public Dictionary<int, string> HelyesSorrend { get => helyesSorrend; set => helyesSorrend = value; }
+		public string Kategoria { get => kategoria; set => kategoria = value; }
+	}
+}
